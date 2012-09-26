779d7437d266b578615ea152528b683575ef6c14
hypercuba <hypercuba@gmail.com>
2012-09-26 23:51:20 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = 'dbc4cb5f24aebd4fdc385929ba3e7067bfe0fff970026ad371302e04d569aefa07179d18f8540b9c3e1fad75bf0c870f0a9585ab6b6eaa07b5c98886645b8bc8'
